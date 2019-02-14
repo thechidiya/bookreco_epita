8f75d7bac33a2b778f47ee10da38c0cbae1a2d4e
Swetali Nimje <thechidiya@swetalis-macbook-air.home>
2019-02-14 23:01:45 +0100

    updated: I'm doing something wrong :/

--- application.py
+++ application.py
@@ -24,7 +24,7 @@ db = scoped_session(sessionmaker(bind=engine))
 def index():
     return render_template("index.html")
 
-@app.route("/login",methods=["GET","POST"] )
+@app.route("/login",methods=["POST"] )
 def login():
     username = request.form.get("username")
     password = request.form.get("passwd")
@@ -35,25 +35,26 @@ def login():
         return render_template("register.html")
 
 @app.route("/register",methods=["GET", "POST"] )
-def register_page():
-    if request.methods == "POST":
-        username = request.form.get("username")
-        email = request.form.get("email")
-        password1 = request.form.get("password1")
-        password2 = request.form.get("password2")
+def register():
+    return render_template("register.html")
 
-        x = db.execute("SELECT * FROM users WHERE (username = username)")
-        if int(x) > 0 or password1 != password2:
-            flash("Username is taken, please select another or password doesn't match")
-            return render_template("register.html")
-        else:
-            db.execute("INSERT INTO users (username, password, email) VALUES (%s, %s, %s)")
-            db.commit()
-            flash("Thanks for registering!")
-            session['logged_in'] = True
-            session['username'] = username
-            return redirect(url_for('dashboard'))
-    return render_template("register.html", form=form)
+@app.route("/register_page",methods=["POST"] )
+def register_page():
+    username = request.form.get("username")
+    email = request.form.get("email")
+    password1 = request.form.get("password1")
+    password2 = request.form.get("password2")
+    x = db.execute("SELECT * FROM users WHERE (username = username)")
+    if int(x) > 0 or password1 != password2:
+        flash("Username is taken, please select another or password doesn't match")
+        return render_template("register.html")
+    else:
+        db.execute("INSERT INTO users (username, password, email) VALUES (%s, %s, %s)")
+        db.commit()
+        flash("Thanks for registering!")
+        session['logged_in'] = True
+        session['username'] = username
+        return redirect(url_for('dashboard'))
 
 @app.route("/dashboard",methods=["GET", "POST"] )
 def dashboard():
