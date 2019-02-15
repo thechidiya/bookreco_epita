5329d00a402f3a5de7c292c1b1a5b3f685383709
Swetali Nimje <thechidiya@swetalis-macbook-air.home>
2019-02-15 03:10:00 +0100

    updated: Fede take in-charge from here now

--- application.py
+++ application.py
@@ -12,6 +12,9 @@ app = Flask(__name__)
 if not os.getenv("DATABASE_URL"):
     raise RuntimeError("DATABASE_URL is not set")
 
+# set up API KEY from goodreads.com
+api_key = os.getenv("API_KEY_GOODREADS")
+
 # Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
@@ -24,30 +27,43 @@ db = scoped_session(sessionmaker(bind=engine))
 def index():
     return render_template("index.html")
 
-@app.route("/login",methods=["POST"] )
+@app.route("/login",methods=["GET", "POST"] )
 def login():
-    username = request.form.get("username")
-    password = request.form.get("passwd")
-    isExist = db.execute("SELECT username FROM users WHERE (username=username) AND (password=password)")
-    if int(isExist) > 0:
-        return render_template("dashboard.html")
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("passwd")
+        isExist = db.execute("SELECT username FROM users WHERE (username=username) AND (password=password)")
+        if int(isExist) > 0:
+            return render_template("dashboard.html")
+        else:
+            return render_template("login.html")
     else:
-        return render_template("register.html")
+        return render_template("login.html")
+
+@app.route("/logout", methods = ['POST', 'GET'])
+def logout():
+    #Log user out
+    # Forget any user_id
+    session.clear()
+    # Redirect user to default
+    return redirect("/")
 
 @app.route("/register",methods=["GET", "POST"] )
 def register():
     return render_template("register.html")
 
-@app.route("/register_page",methods=["POST"] )
-def register_page():
-    username = request.form.get("username")
-    email = request.form.get("email")
-    password1 = request.form.get("password1")
-    password2 = request.form.get("password2")
-    x = db.execute("SELECT * FROM users WHERE (username = username)")
-    if int(x) > 0 or password1 != password2:
-        flash("Username is taken, please select another or password doesn't match")
-        return render_template("register.html")
+@app.route("/register_user",methods=["POST"] )
+def register_user():
+    #register user
+    if request.method == "POST":
+        username = request.form.get("username")
+        email = request.form.get("email")
+        password1 = request.form.get("password1")
+        password2 = request.form.get("password2")
+        x = db.execute("SELECT * FROM users WHERE (username = username)")
+        if int(x) > 0 or password1 != password2:
+            flash("Username is taken, please select another or password doesn't match")
+            return render_template("register.html")
     else:
         db.execute("INSERT INTO users (username, password, email) VALUES (%s, %s, %s)")
         db.commit()
@@ -58,4 +74,14 @@ def register_page():
 
 @app.route("/dashboard",methods=["GET", "POST"] )
 def dashboard():
-    return "To Do"
+    try:
+        db.execute("SELECT * from books")
+        data = db.fetchall()
+        return data
+        
+        return render_template("dashboard.html", data=data)
+        
+    except Exception as e:
+        return (str(e))
+
+
